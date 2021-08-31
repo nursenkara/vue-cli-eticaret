8283fd8a83e190b7884ad423c09ab86387541b09
Necdet UYGUR <necdet.uygur@gmail.com>
2021-08-31 11:40:26 +0300

    ürün detay

--- src/components/UrunDetay.vue
+++ src/components/UrunDetay.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="row">
     <div class="col-md-6">
-      <img style="width: 100%" src="https://picsum.photos/500/400" />
+      <img style="width: 100%" :src="urun.resim" />
     </div>
     <div class="col-md-6">
       <h4>Ürün Detayları</h4>
       <ul class="list-group">
-        <li class="list-group-item">Urun ID: {{ urunId }}</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        <li class="list-group-item">Ürün ID: {{ urunId }}</li>
+        <li class="list-group-item">Ürün Adı: {{ urun.ad }}</li>
+        <li class="list-group-item">Ürün Açıklama: {{ urun.aciklama }}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       urunId: this.$route.params.urunId,
+      urun: {}
     };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/urunler/" + this.urunId)
+      .then((response) => {
+        this.urun = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
 };
 </script>
