53497453b64027ed3a9058e6ebb0287c44e3b286
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-01 18:08:09 +0300

    mitt eklendi

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 import axios from 'axios'
+import mitt from 'mitt'
 
 axios.defaults.baseURL = 'https://json-server-db1.herokuapp.com'
+const emitter = mitt()
 
 const app = createApp(App)
 app.use(router)
+app.config.globalProperties.emitter = emitter
 app.mount('#app')
