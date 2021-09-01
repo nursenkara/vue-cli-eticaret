895ccb86649d7f3d92a1cd46ca3329fccb20eee1
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-02 00:22:36 +0300

    Ürün detay sayfasında görünüm düzeltildi. Sepete ekle butonu eklendi.

--- src/components/UrunDetay.vue
+++ src/components/UrunDetay.vue
@@ -4,26 +4,44 @@
       <img style="width: 100%" :src="urun.resim" />
     </div>
     <div class="col-md-6">
-      <h4>Ürün Detayları</h4>
-      <ul class="list-group">
-        <li class="list-group-item">Ürün ID: {{ urunId }}</li>
-        <li class="list-group-item">Ürün Adı: {{ urun.ad }}</li>
-        <li class="list-group-item">Ürün Fiyatı: {{ urun.fiyat }}</li>
-        <li class="list-group-item">Ürün Açıklama: {{ urun.aciklama }}</li>
-      </ul>
+      <div class="card mb-3">
+        <div class="card-header">Ürün Detayları</div>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ urun.ad }}</li>
+            <li class="list-group-item">{{ urun.aciklama }}</li>
+            <li class="list-group-item">{{ urun.fiyat }}₺</li>
+          </ul>
+        </div>
+        <div class="card-footer">
+          <a
+            href="javascript:void(0);"
+            class="btn btn-primary float-end"
+            @click="sepeteEkle()"
+            >Sepete Ekle
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import ls from "../ls";
 
 export default {
   data() {
     return {
       urunId: this.$route.params.urunId,
-      urun: {}
+      urun: {},
     };
+  },
+  methods: {
+    sepeteEkle() {
+      this.emitter.emit("sepeteEkle", this.urun);
+      this.sepettekiUrunler = ls("sepettekiUrunler");
+    },
   },
   created() {
     axios
