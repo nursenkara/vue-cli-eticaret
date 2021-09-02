e82a49a7f90b1af07293ff14caeeaeb7b5c20c18
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-02 21:43:32 +0300

    Footer eklendi

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
   <div class="container">
     <router-view :key="$route.path"></router-view>
   </div>
+  <Altlik :kategoriler="kategoriler" />
 </template>
 
 <script>
 import Baslik from "./components/Baslik.vue";
+import Altlik from "./components/Altlik.vue";
 import axios from "axios";
 
 export default {
   name: "App",
   components: {
     Baslik,
+    Altlik,
   },
   data() {
     return {
---
+++ src/components/Altlik.vue
@@ -0,0 +1,74 @@
+<template>
+  <footer class="bg-dark text-center text-white">
+    <div class="container p-4">
+      <section class="mb-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+          repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia
+          magnam eum harum corrupti dicta, aliquam sequi voluptate quas.
+        </p>
+      </section>
+      <section class="">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+              <li v-for="kategori in kategoriler" :key="kategori.id">
+                <a
+                  href="javascript:void(0)"
+                  @click="$router.push('/kategori' + kategori.slug)"
+                  class="text-white"
+                  >{{ kategori.ad }}</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+              <li v-for="kategori in kategoriler" :key="kategori.id">
+                <a
+                  href="javascript:void(0)"
+                  @click="$router.push('/kategori' + kategori.slug)"
+                  class="text-white"
+                  >{{ kategori.ad }}</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+              <li v-for="kategori in kategoriler" :key="kategori.id">
+                <a
+                  href="javascript:void(0)"
+                  @click="$router.push('/kategori' + kategori.slug)"
+                  class="text-white"
+                  >{{ kategori.ad }}</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <ul class="list-unstyled mb-0">
+              <li v-for="kategori in kategoriler" :key="kategori.id">
+                <a
+                  href="javascript:void(0)"
+                  @click="$router.push('/kategori' + kategori.slug)"
+                  class="text-white"
+                  >{{ kategori.ad }}</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © {{ new Date().getFullYear() }} Copyright E-Ticaret
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: ["kategoriler"],
+};
+</script>
