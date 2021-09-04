3d6cf0453c5f218e4677532ef93196a862cbf2d2
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-04 17:35:02 +0300

    Loading animasyon yapıldı.

--- src/components/Urunler.vue
+++ src/components/Urunler.vue
@@ -3,7 +3,9 @@
     <Urun :urun="urun" v-for="urun in urunler" :key="urun.id" />
   </div>
   <div class="text-center fs-1 m-5 p-5" v-else>
-    . . .
+    <span class="one">.</span>
+    <span class="two">.</span>
+    <span class="three">.</span>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default {
         .then((response) => {
           this.urunler = response.data;
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
     }
@@ -40,3 +42,53 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.one {
+  opacity: 0;
+  -webkit-animation: dot 1.3s infinite;
+  -webkit-animation-delay: 0s;
+  animation: dot 1.3s infinite;
+  animation-delay: 0s;
+}
+
+.two {
+  opacity: 0;
+  -webkit-animation: dot 1.3s infinite;
+  -webkit-animation-delay: 0.2s;
+  animation: dot 1.3s infinite;
+  animation-delay: 0.2s;
+}
+
+.three {
+  opacity: 0;
+  -webkit-animation: dot 1.3s infinite;
+  -webkit-animation-delay: 0.3s;
+  animation: dot 1.3s infinite;
+  animation-delay: 0.3s;
+}
+
+@-webkit-keyframes dot {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes dot {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
