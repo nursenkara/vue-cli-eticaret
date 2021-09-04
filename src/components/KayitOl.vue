9c32770e124a42097b51c332275d024c05ce741b
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-04 03:36:45 +0300

    MD5 şifreleme yöntemi kullanıldı. Kayıt başarılı mesajı ve girişe yönlendirme yapıldı.

--- src/components/KayitOl.vue
+++ src/components/KayitOl.vue
@@ -5,51 +5,48 @@
         <div class="card-header">
           Kayıt Ol
         </div>
-        <div class="card-body">
+        <div class="card-body" v-if="kayitBasarili">
+          <div>Başarıyla kaydoldunuz!</div>
+          <div>
+            Giriş yapmak için
+            <a href="javascript:void(0)" @click="$router.push('/giris-yap')"
+              >tıklayınız</a
+            >.
+          </div>
+        </div>
+        <div class="card-body" v-if="!kayitBasarili">
           <div class="input-group mb-3">
             <span class="input-group-text">
               Ad
             </span>
-            <input type="text" class="form-control" v-model="kullanici.ad" />
+            <input type="text" class="form-control" v-model="ad" />
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text">
               Soyad
             </span>
-            <input type="text" class="form-control" v-model="kullanici.soyad" />
+            <input type="text" class="form-control" v-model="soyad" />
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text">
               E-Posta
             </span>
-            <input
-              type="text"
-              class="form-control"
-              v-model="kullanici.eposta"
-            />
+            <input type="text" class="form-control" v-model="eposta" />
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text">
               Şifre
             </span>
-            <input
-              type="password"
-              class="form-control"
-              v-model="kullanici.sifre"
-            />
+            <input type="password" class="form-control" v-model="sifre" />
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text">
               Şifre Tekrar
             </span>
-            <input
-              type="password"
-              class="form-control"
-              v-model="kullanici.sifreTekrar"
-            />
+            <input type="password" class="form-control" v-model="sifreTekrar" />
           </div>
         </div>
-        <div class="card-footer">
+        <div class="card-footer" v-if="!kayitBasarili">
           <button class="btn btn-primary float-end" @click="kayitOl()">
             Kayıt Ol
           </button>
@@ -61,27 +58,35 @@
 
 <script>
 import axios from "axios";
+import md5 from "md5";
 
 export default {
   data() {
     return {
-      kullanici: {
-        ad: "",
-        soyad: "",
-        eposta: "",
-        sifre: "",
-        sifreTekrar: ""
-      },
+      kayitBasarili: false,
+      ad: "",
+      soyad: "",
+      eposta: "",
+      sifre: "",
+      sifreTekrar: "",
     };
   },
   methods: {
     kayitOl() {
       if (this.sifre != this.sifreTekrar) {
         alert("Şifreler eşleşmiyor!");
+        return;
       }
-      axios.post("/kullanicilar/", this.kullanici).then(() => {
-        console.log("OK");
-      });
+      axios
+        .post("/kullanicilar/", {
+          ad: this.ad,
+          soyad: this.soyad,
+          eposta: this.eposta,
+          sifre: md5(this.sifre),
+        })
+        .then(() => {
+            this.kayitBasarili = true;
+        });
     },
   },
 };
