d977ac0e8f0be7bec2c033100fb5cb94f6f5f4bf
Necdet UYGUR <necdet.uygur@gmail.com>
2021-09-08 18:31:06 +0300

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <Baslik :kategoriler="kategoriler" />
+
+  <hr />
+  <Modal id="mdl">
+    <button>xxx</button>
+  </Modal>
+  <button @click="a" class="m-3 btn btn-secondary">modal aç</button>
+  <hr />
+
   <div class="container min-vh-100">
     <router-view :key="$route.path"></router-view>
   </div>
@@ -9,13 +17,16 @@
 <script>
 import Baslik from "./components/Baslik.vue";
 import Altlik from "./components/Altlik.vue";
+import Modal from "./components/Modal.vue";
 import axios from "axios";
+import $ from "jquery/src/jquery";
 
 export default {
   name: "App",
   components: {
     Baslik,
     Altlik,
+    Modal,
   },
   data() {
     return {
@@ -31,6 +42,11 @@ export default {
       .catch(function(error) {
         console.log(error);
       });
+  },
+  methods: {
+    a() {
+      $("#mdl").modal("show");
+    },
   },
 };
 </script>
